24d6a059796e713aca29671b12da01df8ec29410
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2022-08-20 00:01:22 +0300

    create project

---
+++ src/test/java/ru/divanov/soapMathWS/SoapMathWsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.divanov.soapMathWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapMathWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
