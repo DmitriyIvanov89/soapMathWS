730bdb42b9e6b028bcc28cf2c0de3953ec80f002
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2022-08-26 18:09:33 +0300

    added quadratic education logic

--- src/main/java/ru/divanov/soapMathWS/server/service/SoapMathServiceImpl.java
+++ src/main/java/ru/divanov/soapMathWS/server/service/SoapMathServiceImpl.java
@@ -15,10 +15,23 @@ public class SoapMathServiceImpl implements SoapMathService {
     @Override
     public GetSolutionQuadraticEquationResponse getSolutionQuadraticEducation(double a, double b, double c) {
         GetSolutionQuadraticEquationResponse response = new GetSolutionQuadraticEquationResponse();
-        response.setFormula("Formula");
-        response.setDiscriminant(a * (b + c));
-        response.setX1(a);
-        response.setX2(b + c);
-        return response;
+
+        if (a > 0) {
+            response.setDiscriminant(Math.pow(b, 2) - 4 * a * c);
+            if (response.getDiscriminant() > 0) {
+                response.setFormula(String.format("%.1fX^2+%.1fX+%.1f=0", a, b, c));
+                response.setX1((-b + Math.sqrt(response.getDiscriminant())) / (2 * a));
+                response.setX2((-b - Math.sqrt(response.getDiscriminant())) / (2 * a));
+                return response;
+            } else if (response.getDiscriminant() == 0) {
+                response.setFormula(String.format("%.1fX^2+%.1fX+%.1f=0", a, b, c));
+                response.setX1(-b / (2 * a));
+                return response;
+            }
+            throw new ArithmeticException("Discriminant < 0"); // my exception
+
+        } else {
+            throw new ArithmeticException("A must be > 0");
+        }
     }
 }
