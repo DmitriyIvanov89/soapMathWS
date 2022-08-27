089e484e0c74d739319fd90cf80395255340d10d
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2022-08-28 00:03:46 +0300

    как же заебался нах

--- src/main/java/ru/divanov/mathws/server/model/GetQuadraticEducationSolution.java
+++ src/main/java/ru/divanov/mathws/server/model/GetQuadraticEducationSolution.java
@@ -13,7 +13,7 @@ import javax.xml.bind.annotation.XmlType;
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "getSolutionQuadraticEducation", propOrder = {"a", "b", "c"})
 public class GetQuadraticEducationSolution {
-    @XmlElement(name = "A", required = true, defaultValue = "0")
+    @XmlElement(name = "A", required = true)
     private Double a;
 
     @XmlElement(name = "B", required = true)
--- src/main/java/ru/divanov/mathws/server/service/MathWebServiceImpl.java
+++ src/main/java/ru/divanov/mathws/server/service/MathWebServiceImpl.java
@@ -19,21 +19,21 @@ public class MathWebServiceImpl implements MathWebService {
     public GetSolutionQuadraticEquationResponse getSolutionQuadraticEducation(GetQuadraticEducationSolution request) throws MathException {
         GetSolutionQuadraticEquationResponse response = new GetSolutionQuadraticEquationResponse();
 
+        String formula = String.format("%.1fX^2+%.1fX+%.1f=0", request.getA(), request.getB(), request.getC());
+
         if (request.getA() != 0) {
             response.setDiscriminant(Math.pow(request.getB(), 2) - 4 * request.getA() * request.getC());
             if (response.getDiscriminant() > 0) {
-                response.setFormula(String.format("%.1fX^2+%.1fX+%.1f=0", request.getA(), request.getB(), request.getC()));
+                response.setFormula(formula);
                 response.setX1((-request.getB() + Math.sqrt(response.getDiscriminant())) / (2 * request.getA()));
                 response.setX2((-request.getB() - Math.sqrt(response.getDiscriminant())) / (2 * request.getA()));
                 return response;
             } else if (response.getDiscriminant() == 0) {
-                response.setFormula(String.format("%.1fX^2+%.1fX+%.1f=0", request.getA(), request.getB(), request.getC()));
+                response.setFormula(formula);
                 response.setX1(-request.getB() / (2 * request.getA()));
                 return response;
             }
-            throw new MathException("Discriminant is less than 0",
-                    new QuadraticEducationFault(String.format("%.1fx^2+%.1fX+%.1f=0", request.getA(), request.getB(), request.getC()),
-                            response.getDiscriminant()));
+            throw new MathException("Discriminant is less than 0", new QuadraticEducationFault(formula, response.getDiscriminant()));
         } else {
             throw new MathException("Coefficient 'A' mustn't be equal 0");
         }
